dc539ac37a39d62c9ef4a08b063054abf0896000
Serhii Zhitnik <135828574+allhitsugaya@users.noreply.github.com>
2025-05-20 19:01:41 +0300

    update .gitignore

--- src/services/supabaseClient.js
+++ src/services/supabaseClient.js
@@ -3,7 +3,7 @@ import { createClient } from '@supabase/supabase-js'
 
 const token = import.meta.env.VITE_TOKEN
 export const supabaseUrl = 'https://rfhtbdhpgwsvvxfkcyvu.supabase.co'
-const supabaseKey = token
+const supabaseKey = `${token}`
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 export default supabase;
